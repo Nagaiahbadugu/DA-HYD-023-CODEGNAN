4eab4705f0d83e99940b8242e3444b1822ecf9c5
Nagaiahbadugu <nagaiahbadugu123@gmail.com>
2026-07-28 15:53:14 +0530

    nag

---
+++ day1.py
@@ -0,0 +1,34 @@
+#IDLE is color coding environment
+#hastags is used as single line comment...
+#we use triple quotes(single/double)''',""" as multiline comments
+
+#print('Hello Codegnan')
+
+
+#tokens --> a smallest unit in a program.there are 5 types of tokens..
+
+#->keywords
+#->identifiers
+#->literals
+#->operators
+#->punctuators
+'''
+name ='nagaiah'
+print(name)
+email_id ='nagaiahbadugu123@gmail.com'
+#print(email_id) #python is case sensitive -->nameerror
+#1swer=23
+#print(1swer)
+#@defrrrrr=3.4 #no special characters
+
+#true =45 #no keywords
+
+batch="da23"
+print(batch)
+
+'''
+name,location,age='codegnan','hyd',7
+print(name)
+print(location)
+print(age)
+
