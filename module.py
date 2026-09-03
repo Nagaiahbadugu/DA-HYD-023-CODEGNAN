e5beeadfb37bf0f73f23968236c38c6876c8003b
Nagaiahbadugu <nagaiahbadugu123@gmail.com>
2026-09-03 10:43:02 +0000

    moduleusage

---
+++ module.py
@@ -0,0 +1,67 @@
+'''import data_abstraction
+print(dir(data_abstraction))
+print(type(data_abstraction))
+print(type(data_abstraction))
+
+print(data_abstraction.greeting())
+print(data_abstraction)
+
+data_abstraction['subjects']=['python','SQL','EDA','powerbi']
+print(data_abstraction.keys())
+
+from data_abstraction import details
+print(details)
+
+
+from data_abstractio import details,greeting
+print(greeting())
+print(details)
+from data_abstraction import*
+print(details)
+print(greeting())
+
+import data_abstraction as mod
+print(mod.details)
+'''
+import random,time
+print(dir(random))
+
+for i in range (5):
+    print(random.randint(1000,9999))
+    time.sleep(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
