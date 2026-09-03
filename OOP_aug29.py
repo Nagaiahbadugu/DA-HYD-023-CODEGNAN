5f1122e6beb73f4afb4fdc37503d8a81c87619b7
Nagaiahbadugu <nagaiahbadugu123@gmail.com>
2026-09-03 10:38:58 +0000

    introductiontomodules

---
+++ OOP_aug29.py
@@ -0,0 +1,31 @@
+
+def greeting():
+    """simple greeting function"""
+    return "welcome to codegnan"
+
+
+details = {'company':'codegnan','batch': 'DA-23','place':'hyd'}
+
+if __name__=="__main__":
+    (greeting())
+
+(__name__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
